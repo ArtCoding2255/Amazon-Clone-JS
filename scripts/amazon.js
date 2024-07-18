6b914a1297afd49b1835326720a6921a84ae6185
Katepiss Sriburapa <artcoding2544@gmail.com>
2024-07-18 11:37:41 +0700

    feat: use product.js to get product data

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,45 +1,48 @@
-const products = [
-  {
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090, //1 dollar = 100 cents
-  },
-  {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095, //1 dollar = 100 cents
-  },
-  {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799, //1 dollar = 100 cents
-  },
-  {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: '55555555555555555555',
-    rating: {
-      stars: 5,
-      count: 60,
-    },
-    priceCents: 799, //1 dollar = 100 cents
-  },
-];
+// const products = [
+//   {
+//     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating: {
+//       stars: 4.5,
+//       count: 87,
+//     },
+//     priceCents: 1090, //1 dollar = 100 cents
+//   },
+//   {
+//     image: 'images/products/intermediate-composite-basketball.jpg',
+//     name: 'Intermediate Size Basketball',
+//     rating: {
+//       stars: 4,
+//       count: 127,
+//     },
+//     priceCents: 2095, //1 dollar = 100 cents
+//   },
+//   {
+//     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating: {
+//       stars: 4.5,
+//       count: 56,
+//     },
+//     priceCents: 799, //1 dollar = 100 cents
+//   },
+//   {
+//     image: 'images/products/black-2-slot-toaster.jpg',
+//     name: '2 Slot Toaster - Black',
+//     rating: {
+//       stars: 5,
+//       count: 2179,
+//     },
+//     priceCents: 1899, //1 dollar = 100 cents
+//   },
+// ];
 
 //--- Combine the HTML of these products html --
+
+//ther
 let productsHtml = '';
 
+// products variable is come from data/products.js file
 products.forEach((product) => {
   productsHtml += `
    <div class="product-container">
@@ -64,7 +67,7 @@ products.forEach((product) => {
             }</div>
           </div>
 
-          <div class="product-price">${(product.priceCents / 100).toFixed(
+          <div class="product-price">$${(product.priceCents / 100).toFixed(
             2
           )}</div>
 
