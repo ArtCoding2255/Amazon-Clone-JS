615fb1ef480bdaea94cb978d4f579257a3b85b5b
Katepiss Sriburapa <artcoding2544@gmail.com>
2024-07-27 20:31:14 +0700

    feat: Fix radio selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,6 +45,8 @@
 
 import { cart, addToCart } from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from '../scripts/utils/money.js';
+
 let productsHtml = '';
 // products variable is come from data/products.js file
 
@@ -72,8 +74,8 @@ products.forEach((product) => {
             }</div>
           </div>
 
-          <div class="product-price">$${(product.priceCents / 100).toFixed(
-            2
+          <div class="product-price">$${formatCurrency(
+            product.priceCents
           )}</div>
 
           <div class="product-quantity-container">
